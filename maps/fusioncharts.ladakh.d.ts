655cec6d6be26c2e9acf56327ff6bdbb44d3e3fb
sanjay.bhan <sanjay.bhan@accoliteindia.com>
2022-08-05 15:05:00 +0530

    changes w.r.t 3.19.0

---
+++ maps/fusioncharts.ladakh.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Ladakh {}
+declare var Ladakh: (H: FusionChartStatic) => FusionChartStatic;
+export = Ladakh;
+export as namespace Ladakh;
+
